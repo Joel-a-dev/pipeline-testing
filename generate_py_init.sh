1d1f4ab74d3f989202d607e07a57a59167280c2c
Joel Almeida <joel.almeida@ibm.com>
2019-01-24 16:34:34 +0000

    node testing

--- generate_py_init.sh
+++ generate_py_init.sh
@@ -8,8 +8,17 @@
 if $(git describe --tags --abbrev=0); then
     GHE_VERSION="$(git describe --tags --abbrev=0)"
 else
-    GHE_VERSION="${BRANCH_NAME}-$(git rev-parse HEAD | head -c 7)"
+    CURRENT_VERSION=$(cat package.json \
+                    | grep version \
+                    | head -1 \
+                    | awk -F: '{ print $2 }' \
+                    | sed 's/[",]//g'
+                    | tr -d '[[:space:]]')
+
+    GHE_VERSION="${CURRENT_VERSION}-${BRANCH_NAME}-$(git rev-parse HEAD | head -c 7)"
 fi
+
+npm version "${GHE_VERSION}"
 
 GIT_COMMIT=$(git rev-parse HEAD)
 BUILD_TIMESTAMP=$(date +'%Y-%m-%dT%H:%M:%SZ')
